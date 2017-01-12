23f2c026c38801cf537fbc54a7803a57268e6555
unknown <2283142s@BO1028-6-07.ad.dcs.gla.ac.uk>
2017-01-12 10:30:11 +0000

    Adding about page, updating gitignore

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello rango!")
+    return HttpResponse("<a href='about'>Hello rango!</a>")
+
+def about(request):
+    return HttpResponse("About page");
