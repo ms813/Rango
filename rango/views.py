e6ae75b8238f436efee4fd7fcb4c9c5ab0b38683
unknown <2283142s@BO1028-7-02.ad.dcs.gla.ac.uk>
2017-01-19 10:02:42 +0000

    Adding media directory. Updating about page to be a template with images in the static and media directories

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,5 @@ def index(request):
     return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
-    return HttpResponse("About page");
+    context_dict = {'author' : 'Matthew Smith'}
+    return render(request, 'rango/about.html', context = context_dict)
