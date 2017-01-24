be9beaae4221e1f5d56b549e161de38822a7402d
= <matthew.smith813@gmail.com>
2017-01-24 15:24:50 +0000

    Completing ch6 exercises

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,9 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]    
-    context_dict = {'categories': category_list}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages' : page_list}
 
     return render(request, 'rango/index.html', context_dict)
 
